26e922c0d17eab3e5a88d37b849dea03cdbea5db
yash suvagiya <yashsuvagiya112@gmail.com>
2023-05-02 12:46:32 +0530

    assetimageandfont

--- lib/assetimage_demo.dart
+++ lib/assetimage_demo.dart
@@ -10,10 +10,18 @@ class AssetimagedDemo extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/Screenshot 2023-04-30 022233.png')))),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/images/Screenshot 2023-04-30 022233.png',
+              ),
+            ),
+          ),
+          child: const Text(
+            'yash',
+            style: TextStyle(fontFamily: 'Schyler', color: Colors.white),
+          ),
+        ),
       ),
     );
   }
