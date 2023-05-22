bd5bf6150f8e3eca57e2508c8c4f693841884084
yash suvagiya <yashsuvagiya112@gmail.com>
2023-05-22 10:38:11 +0530

    smarthome(continue)

---
+++ lib/tranformrotate_ui.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TranformRotate extends StatelessWidget {
+  const TranformRotate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Row(
+              children: [
+                Transform.rotate(
+                  angle: 60,
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 130,
+                    ),
+                    height: 150,
+                    width: 150,
+                    color: Colors.orange,
+                    child: RotatedBox(
+                      quarterTurns: 7,
+                      child: Text(
+                        'yash',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.red,
+              ),
+              const Spacer(
+                flex: 2,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
