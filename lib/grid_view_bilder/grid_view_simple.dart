b6b8059c3a3d08b776c64dcf19cbfb28814ab57e
yash suvagiya <yashsuvagiya112@gmail.com>
2023-06-10 12:44:20 +0530

    gridview(complite)

---
+++ lib/grid_view_bilder/grid_view_simple.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:my_practice/grid_view_bilder/list_gridview_demo.dart';
+
+class SimpleGridView extends StatelessWidget {
+  const SimpleGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 03,
+                mainAxisSpacing: 06,
+                crossAxisSpacing: 10,
+              ),
+              children: List.generate(
+                gridData.length,
+                (index) => Container(
+                  color: Colors.green,
+                  child: Column(
+                    children: [
+                      Icon(gridData[index]['icon']),
+                      Text(gridData[index]['name']),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_practice/grid_view_bilder/grid_view_extent.dart';
+import 'package:my_practice/grid_view_bilder/grid_view_simple.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GridViewExtendDemo(),
+      home: SimpleGridView(),
     );
   }
 }
